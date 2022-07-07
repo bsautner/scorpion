9645eada13c93f47e25aa585371de2160cd29e6a
Benjamin Sautner <1295207+bsautner@users.noreply.github.com>
2022-07-07 06:23:19 -0400

    Update mind.sh

--- setup/mind.sh
+++ setup/mind.sh
@@ -8,3 +8,16 @@ sudo update-alternatives --install "/usr/bin/java" "java" "/usr/lib/jvm/jdk-11.0
 sudo update-alternatives --install "/usr/bin/javac" "javac" "/usr/lib/jvm/jdk-11.0.15/bin/javac" 0
 sudo update-alternatives --set java /usr/lib/jvm/jdk-11.0.15/bin/java
 sudo update-alternatives --set javac /usr/lib/jvm/jdk-11.0.15/bin/javac
+
+sudo apt install -y git
+
+sudo apt install -y mosquitto
+sudo systemctl enable mosquitto
+sudo systemctl restart mosquitto
+sudo systemctl status mosquitto
+sudo apt install -y mosquitto-clients
+
+
+
+sudo apt-get install -y python-smbus
+sudo apt-get install -y python3-pip
