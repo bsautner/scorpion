2aeb6fd41f8bcac2f618550abbe4aa457363ca2b
Benjamin Sautner <ben@sautner.me>
2022-07-07 12:49:28 -0400

    transcribe

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ git pull
 cd display
 ./gradlew assemble shadowJar
 cd ./build/libs
-java -cp display-1.0-SNAPSHOT-all.jar MainKt
+java -cp display-1.0-SNAPSHOT-all.jar MainKt & python3 /home/ben/scorpion/python/main/transcribe/transcribe.py $
