0d895b4df565a46f6ea5d8800e4a67d61e7777cf
Benjamin Sautner <1295207+bsautner@users.noreply.github.com>
2022-09-01 10:24:38 -0400

    Update pi_01.sh

--- setup/pi_01.sh
+++ setup/pi_01.sh
@@ -31,7 +31,7 @@ cd ~
 sudo pip3 install --upgrade adafruit-python-shell
 wget https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/raspi-blinka.py
 sudo python3 raspi-blinka.py
-
+pip3 install adafruit-circuitpython-rplidar
 cd ~
 git clone https://github.com/bsautner/scorpion.git
 cp ./scorpion/run.sh ~
